49a355ba2153110a97c431903df26e48a2c150c2
5reStudios <clauderobbindev@gmail.com>
2024-09-28 10:57:13 +0530

    skull walks now

--- scripts/villain.gd
+++ scripts/villain.gd
@@ -6,7 +6,7 @@ var movingRight = 1
 var canSwitch = true
 func _physics_process(delta: float) -> void:
 	if !$RayCast2D.is_colliding() and canSwitch:
-		movingRight = 1
+		movingRight *= 1
 		canSwitch = false
 	else:
 		canSwitch =true
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	
 	
 	if movingRight <0:
-		velocity.x = SPEED * -1
+		velocity.x = SPEED * -1.0
 		$RayCast2D.target_position = Vector2(-270,250)
 	else:
 		velocity.x = SPEED * 1.0
@@ -22,3 +22,8 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 	if velocity.x > 0:
 		$AnimatedSprite2D.animation = "skullwalking"
+
+
+
+func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass
